0cee0366a045086ee071443b6ae5ac3021cde531
Mero <mr_chens@outlook.com>
2018-12-11 11:07:29 +0800

    fix script grammer error

--- lldb_python/panda/pd_commands.py
+++ lldb_python/panda/pd_commands.py
@@ -1,4 +1,4 @@
 def pdBreak(debugger, command, result, internal_dict):
         # the follow command show how to write break command
-        # debugger,HandleCommand('br s -r \'\[BluetoothVC babyDelegate\]$\'')
+        # debugger.HandleCommand('br s -r \'\[BluetoothVC babyDelegate\]$\'')
         # debugger.HandleCommand('br s -n \'[NSData(AESAdditions) AES256EncryptWithKey:iv:]\'')
